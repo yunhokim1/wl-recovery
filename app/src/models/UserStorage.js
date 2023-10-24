c258ec9add3251a89d19df1fe8e48830cd005791
yunhokim1 <114965423+yunhokim1@users.noreply.github.com>
2023-10-24 19:04:56 +0900

    회원가입 라우팅 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -30,6 +30,15 @@ class UserStroage {
 
         return userInfo;
     }
+
+    static save(userInfo) {
+        const users = this.#users;
+        users.id.push(userInfo.id);
+        users.name.push(userInfo.name);
+        users.psword.push(userInfo.psword);
+        
+        return { success: true};
+    }
 }
 
 module.exports = UserStroage;
